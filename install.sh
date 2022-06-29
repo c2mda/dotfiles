352b49197ba3aa0492b3fd8f46317bded20bab76
Cyprien de Masson <cyprien.de.masson@gmail.com>
2022-06-29 14:06:40 +0200

    Copy files instead of linking.

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 folder=$(dirname -- "$0";)
 
-maybe_link () {
+maybe_copy () {
   from=$1
   to=$2
-  if [ -e $from ] || [ -L $from ]; then
-    echo "File $from already exists, comparing."
+  if [ -e $to] || [ -L $to ]; then
+    echo "File $to already exists, comparing."
     if ! cmp $from $to >/dev/null 2>&1; then
-      echo "Files $from and $to differ, removing $from."
-      rm $from
-      echo "Linking from $from to $to."
-      ln -s $to $from
+      echo "Files $from and $to differ, removing $to."
+      rm $to
+      echo "Copying from $from to $to."
+      cp $from $to
     else
       echo "Files $from and $to match, skipping."
     fi
   else
-    echo "File $from doesn't exist, linking from $from to $to."
-    ln -s $to $from
+    echo "File $to doesn't exist, copying from $from to $to."
+    cp $from $to
   fi
 }
 
 git config --global user.email "cyprien.de.masson@gmail.com"
 git config --global user.name "Cyprien de Masson"
 
-maybe_link ~/.inputrc ${folder}/.inputrc
-maybe_link ~/.vimrc ${folder}/.vimrc
-maybe_link ~/.bashrc ${folder}/.bashrc
+maybe_link ${folder}/.inputrc ~/.inputrc 
+maybe_link ${folder}/.vimrc ~/.vimrc 
+maybe_link ${folder}/.bashrc ~/.bashrc 
