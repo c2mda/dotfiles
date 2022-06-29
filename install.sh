e2e257301a4f66a85ac2c04e661931e41b5228dc
Cyprien de Masson <cyprien.de.masson@gmail.com>
2022-06-29 13:58:11 +0200

    Configure git, compare properly, remove correct file

--- install.sh
+++ install.sh
@@ -3,24 +3,25 @@ folder=$(dirname -- "$0";)
 maybe_link () {
   from=$1
   to=$2
-  echo "Linking from $from to $to" 
-
-  if [[ -f $from ]]; then
+  if [ -e $from ] || [ -L $from ]; then
     echo "File $from already exists, comparing."
     if ! cmp $from $to >/dev/null 2>&1; then
       echo "Files $from and $to differ, removing $from."
-      rm ~/.inputrc
+      rm $from
       echo "Linking from $from to $to."
-      ln -s $to $from 
+      ln -s $to $from
     else
       echo "Files $from and $to match, skipping."
     fi
   else
-    echo "Linking from $from to $to."
-    ln -s $to $from 
+    echo "File $from doesn't exist, linking from $from to $to."
+    ln -s $to $from
   fi
 }
 
-maybe_link ~/.inputrc ${folder}/.inputrc  
+git config --global user.email "cyprien.de.masson@gmail.com"
+git config --global user.name "Cyprien de Masson"
+
+maybe_link ~/.inputrc ${folder}/.inputrc
 maybe_link ~/.vimrc ${folder}/.vimrc
 maybe_link ~/.bashrc ${folder}/.bashrc
