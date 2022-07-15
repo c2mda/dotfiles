059287aed151110e5eb67585047a5527465e7b95
Cyprien de Masson <cyprien.de.masson@gmail.com>
2022-07-15 11:57:52 +0000

    Install FZF, source bashrc

--- install.sh
+++ install.sh
@@ -37,3 +37,10 @@ fi
 
 # Install vim plugins.
 vim +PluginInstall +qall
+
+# Install FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Source bashrc
+source ~/.bashrc
