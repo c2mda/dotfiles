e938b83949b39d8e103bdf743069e86dda0f7725
Cyprien de Masson <cyprien.de.masson@gmail.com>
2022-10-10 22:22:14 +0100

    remove some nesting.

--- sync_backups.sh
+++ sync_backups.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 backup_disks=( /Volumes/backup*/ )
 num_backups=${#backup_disks[*]}
 echo "Found ${num_backups} disks: ${backup_disks[@]}"
@@ -10,46 +12,55 @@ fi
 
 for disk_from in "${backup_disks[@]}"; do
   for disk_to in "${backup_disks[@]}"; do
-    if [ $disk_from != $disk_to ]; then 
-      echo "Dry run ${disk_from} -> ${disk_to}"
-      read -n 1 -s
 
-      rsync \
-        --perms \
-        --whole-file \
-        --chmod=777 \
-        --archive \
-        --recursive \
-        --stats \
-        --human-readable \
-        --exclude=".*" \
-        --out-format="/%f" \
-        --dry-run \
-        $disk_from \
-        $disk_to \
-        | egrep -v "${disk_from}[^/]+/[^/]+/"  # Don't print all subfolders.
-
-      read -p "Proceed ${disk_from} -> ${disk_to}? " -n 1 -r
-      echo    # (optional) move to a new line
-
-      if [[ $REPLY =~ ^[Yy]$ ]]; then
-        rsync \
-          --perms \
-          --whole-file \
-          --chmod=777 \
-          --archive \
-          --recursive \
-          --stats \
-          --human-readable \
-          --exclude=".*" \
-          --progress \
-          --info=name0 \
-          --info=progress2 \
-          $disk_from \
-          $disk_to
-      fi
-
-      echo -e "\n\n\n"
+    # Don't sync a disk with itself.
+    if [[ $disk_from == $disk_to ]]; then 
+      continue
     fi
+
+    # Dry run.
+    echo "Dry run ${disk_from} -> ${disk_to}"
+    read -n 1 -s
+
+    rsync \
+      --perms \
+      --whole-file \
+      --chmod=777 \
+      --archive \
+      --recursive \
+      --stats \
+      --human-readable \
+      --exclude=".*" \
+      --out-format="/%f" \
+      --dry-run \
+      $disk_from \
+      $disk_to \
+      | egrep -v "${disk_from}[^/]+/[^/]+/"  # Don't print subfolders.
+
+    # Confirmation.
+    read -p "Proceed ${disk_from} -> ${disk_to}? " -n 1 -r
+    echo    # Move to a new line.
+
+    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+      continue
+    fi
+
+    # Real run.
+    rsync \
+      --perms \
+      --whole-file \
+      --chmod=777 \
+      --archive \
+      --recursive \
+      --stats \
+      --human-readable \
+      --exclude=".*" \
+      --progress \
+      --info=name0 \
+      --info=progress2 \
+      $disk_from \
+      $disk_to
+
+    echo -e "\n\n\n"
   done
 done
