224bc3a8e41b47b4e76196cd5a75eda6d250ddc9
Cyprien de Masson <cyprien.de.masson@gmail.com>
2023-07-17 14:23:18 +0000

    Extract function for apt install, reorg.

--- install.sh
+++ install.sh
@@ -24,6 +24,25 @@ maybe_copy () {
   fi
 }
 
+# To avoid running apt-get update every time.
+apt_updated=false
+
+function maybe_apt_install() {
+  package_name=$1
+  if ! dpkg-query --show --showformat='${db:Status-Status}\n' python3.8-venv &> /dev/null; then
+    echo "Installing python3.8-venv"
+
+    if [ ! "${apt_updated}" = true ]; then
+      apt-get -qq -o=Dpkg::Use-Pty=0Q update
+      apt_updated=true
+    fi
+    apt-get -qq -o=Dpkg::Use-Pty=0Q install --no-upgrade "${package_name}"
+  else
+    # Nothing installed.
+    return 1
+  fi
+}
+
 # Note: also needs to upload ssh private key.
 git config --global user.email "cyprien.de.masson@gmail.com"
 git config --global user.name "Cyprien de Masson"
@@ -52,15 +71,21 @@ if [[ ! -a "$HOME/.fzf" ]]; then
   source ~/.bashrc
 fi
 
-if ! dpkg-query --show --showformat='${db:Status-Status}\n' fd-find &> /dev/null; then
-  echo "Installing fdfind and aws cli"
-  sudo apt-get -qq -o=Dpkg::Use-Pty=0Q update
-  sudo apt-get -qq -o=Dpkg::Use-Pty=0Q install --no-upgrade fd-find awscli
+# Debian packages.
+maybe_apt_install "build-essential"
+maybe_apt_install "cmake"
+maybe_apt_install "vim-nox"
+maybe_apt_install "python3-dev"
+maybe_apt_install "python3.8-venv"
+maybe_apt_install "fd-find"
+maybe_apt_install "awscli"
+maybe_apt_install "python3-pip"
+if $?; then
+  pip install --quiet autopep8 reorder-python-imports pylint black ruff
 fi
 
-# Setup vim swap folder.
+# Vim
 mkdir -p ~/.vim/swap
-
 if [ ! -e ~/.vim/autoload/plug.vim ]; then
   echo "Installing vimplug"
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -72,35 +97,19 @@ if [ ! -e ~/.vim/autoload/plug.vim ]; then
   # Some stuff needed for YouCompleteMe in vim.
   # A bit heavy but couldn't find a good lighter autocomplete.
   if ! ( ls ~/.vim/plugged/YouCompleteMe/third_party/ycmd/ycm_core.*.so &> /dev/null ) ; then
-    sudo apt-get -qq -o=Dpkg::Use-Pty=0Q update
-    sudo apt-get -qq -o=Dpkg::Use-Pty=0Q install --no-upgrade build-essential \
-      cmake vim-nox python3-dev
+    cmake vim-nox python3-dev
     cd ~/.vim/plugged/YouCompleteMe
     python3 install.py
   fi
 fi
 
-if ! dpkg-query --show --showformat='${db:Status-Status}\n' python3-pip &> /dev/null; then
-  echo "Installing pip"
-  sudo apt-get -qq -o=Dpkg::Use-Pty=0Q update
-  sudo apt-get -qq -o=Dpkg::Use-Pty=0Q install --no-upgrade python3-pip
-  # Required for Python3 formatting.
-  pip install --quiet autopep8 reorder-python-imports pylint black ruff
-fi
-
+# Cloud
 if [[ ! -a /usr/local/bin/kubectl ]]; then
   echo "Installing kubectl"
   curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
   chmod +x ./kubectl
-  sudo mv ./kubectl /usr/local/bin/kubectl
-  sudo mkdir -p ~/.kube
-fi
-# sudo cp /mnt/volumetrialcyp/cw-kubeconfig ~/.kube/config
-
-if ! dpkg-query --show --showformat='${db:Status-Status}\n' python3.8-venv &> /dev/null; then
-  echo "Installing python3.8-venv"
-  sudo apt-get -qq -o=Dpkg::Use-Pty=0Q update
-  sudo apt-get -qq -o=Dpkg::Use-Pty=0Q install --no-upgrade python3.8-venv
+  mv ./kubectl /usr/local/bin/kubectl
+  mkdir -p ~/.kube
 fi
 
 if ! command -v oci --version &> /dev/null; then
