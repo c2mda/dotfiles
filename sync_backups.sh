aa803a1a7c838a258e0b61920e320d9b5b6b1a65
Cyprien de Masson <cyprien.de.masson@gmail.com>
2023-11-16 12:07:13 +0000

    extract func

--- sync_backups.sh
+++ sync_backups.sh
@@ -20,52 +20,59 @@ fi
 
 backup_disks=( /Volumes/backup*/ )
 num_backups=${#backup_disks[*]}
-echo "Found ${num_backups} disks: ${backup_disks[@]}"
+echo "Found ${num_backups} disks: ${backup_disks[*]}"
 
-if [ $num_backups -lt 2 ]; then
-  echo "Need at least two backup disks, found: $backup_disks"
+if [ "$num_backups" -lt 2 ]; then
+  echo "Need at least two backup disks, found: ${backup_disks[*]}"
   exit
 fi
+
+function do_rsync(){
+  # --perms: preserve permissions.
+  # --recursive: copy subfolders.
+  # --times: preserve times.
+  # --whole-file: files are transferred whole, not delta.
+  # --stats: print stats at the end (number/size/etc).
+  # --exclude=".*": exclude hidden files .
+  rsync \
+      --perms \
+      --recursive \
+      --times \
+      --whole-file \
+      --stats \
+      --exclude=".*" \
+      --chmod=777 \
+      --human-readable \
+      "$@"
+}
 
 for disk_from in "${backup_disks[@]}"; do
   for disk_to in "${backup_disks[@]}"; do
 
     # Don't sync a disk with itself.
-    if [[ $disk_from == $disk_to ]]; then
+    if [[ "$disk_from" == "$disk_to" ]]; then
       continue
     fi
 
     # Dry run.
-    read -p "Dry run ${disk_from} -> ${disk_to} ? (Y/y)" -n 1 -r
-    echo    # Move to a new line.
-    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+    read -p "Dry run ${disk_from} -> ${disk_to} ? (y) " -n 1 -r
+    echo "y"   # Move to a new line.
+    if ! [[ ${REPLY:-y} =~ ^[Yy]$ ]]; then
       continue
     fi
 
-    # --perms: preserve permissions.
-    # --recursive: copy subfolders.
-    # --times: preserve times.
-    # --whole-file: files are transferred whole, not delta.
-    # --stats: print stats at the end (number/size/etc).
-    rsync \
-      --perms \
-      --recursive \
-      --times \
-      --whole-file \
-      --chmod=777 \
-      --stats \
-      --human-readable \
-      --exclude=".*" \
+    # --out-format: print full path 
+    do_rsync \
       --out-format="/%f" \
       --dry-run \
-      $disk_from \
-      $disk_to \
-      | egrep -v "${disk_from}[^/]+/[^/]+/"  # Don't print subfolders.
+      "$disk_from" \
+      "$disk_to" \
+      | grep -E -v "${disk_from}[^/]+/[^/]+/"  # Don't print subfolders.
 
     # Confirmation.
-    read -p "Proceed ${disk_from} -> ${disk_to}? (Y/y)" -n 1 -r
-    echo    # Move to a new line.
-    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+    read -p "Proceed ${disk_from} -> ${disk_to}? (y/n) " -n 1 -r
+    echo "y"   # Move to a new line.
+    if ! [[ ${REPLY:-y} =~ ^[Yy]$ ]]; then
       continue
     fi
 
@@ -73,20 +80,12 @@ for disk_from in "${backup_disks[@]}"; do
     # --no-inc-recursive: build full file list first, for exact total progress.
     # --info=name0: don't display current name.
     # --info=progress2: accurate total progress.
-    rsync \
-      --perms \
-      --recursive \
-      --times \
-      --whole-file \
+    do_rsync \
       --no-inc-recursive \
-      --chmod=777 \
-      --stats \
-      --human-readable \
-      --exclude=".*" \
       --info=name0 \
       --info=progress2 \
-      $disk_from \
-      $disk_to
+      "$disk_from" \
+      "$disk_to"
 
     echo -e "\n\n\n"
   done
